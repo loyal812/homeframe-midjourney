27306eafc3570c0a033a2211d7498d06a0fceb37
loyal812 <jh.chan0812@gmail.com>
2024-09-04 13:07:34 -0400

    fix: main slider

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -1,8 +1,19 @@
+
+import CartSVG from "../components/cartImg"
+import EditIcon from "../components/editIcon"
+import StyleSelect from "../components/style_select"
+import PromptDiv from "../components/promptDiv"
+import PriceDiv from "../components/priceDiv"
+
 const Footer = () => {
     return (
-        <div className=" fixed">
-
+        <>
+        <div className="footer">
+            <StyleSelect />
+            <PromptDiv />
+            <PriceDiv />
         </div>
+        </>
     )
 }
 
--- src/views/test/index.jsx
+++ src/views/test/index.jsx
@@ -1,9 +1,47 @@
-const TestView = () => {
-    return (
-        <>
-            <div className="relative">TEst</div>
-        </>
-    )
-}
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
-export default TestView;
+const Slider = () => {
+    return (
+        <div className='main'>
+            <Splide options={{
+                rewind: 'true',
+                type: 'loop',
+                perPage: 3,
+                focus: 'center', gap: 40
+            }} aria-label="React Splide Example">
+                <SplideSlide>
+                    <div className='component'>
+                        <div className='frame'>
+                            <img src='/assets/img/colorDefault.jpeg' />
+                        </div>
+                        <div className='image'>
+                            <img src='/assets/img/1.png' />
+                        </div>
+                    </div>
+                </SplideSlide>
+                <SplideSlide>
+                    <div className='component'>
+                        <div className='frame'>
+                            <img src='/assets/img/colorDefault.jpeg' />
+                        </div>
+                        <div className='image'>
+                            <img src='/assets/img/2.png' />
+                        </div>
+                    </div>
+                </SplideSlide>
+                <SplideSlide>
+                    <div className='component'>
+                        <div className='frame'>
+                            <img src='/assets/img/colorDefault.jpeg' />
+                        </div>
+                        <div className='image'>
+                            <img src='/assets/img/3.png' />
+                        </div>
+                    </div>
+                </SplideSlide>
+            </Splide>
+        </div>
+    );
+}
+export default Slider;
